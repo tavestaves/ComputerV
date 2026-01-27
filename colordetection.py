3688519d1df2de7080de266fca1e75348bbae72b
Taves <mrteverah@gmail.com>
2026-01-27 04:32:48 +0200

    Init

--- colordetection.py
+++ colordetection.py
@@ -1,10 +1,11 @@
+from cmath import pi
 import cv2 
 from utils import get_limits
 from PIL import Image
 
 
 
-pink = [255, 0, 255] #pink in BGR
+yellow = [255, 255, 0] # BGR Format
 
 cap = cv2.VideoCapture(0)
 
@@ -14,7 +15,7 @@ while True:
 
     hsv_img = cv2.cvtColor(frames, cv2.COLOR_BGR2HSV)
 
-    lower_limit, upper_limit = get_limits(pink)
+    lower_limit, upper_limit = get_limits(yellow)
 
     mask = cv2.inRange(hsv_img, lower_limit, upper_limit)
     
@@ -28,6 +29,7 @@ while True:
         cv2.rectangle(frames, (x, y), (w, h), (0, 255, 0), 2)
 
     cv2.imshow('Webcam', frames)
+    cv2.imshow('Mask', mask)
 
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
